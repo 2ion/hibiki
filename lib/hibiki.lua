ead185adcdb5e50ae2acc2ee9d150327422e41b4
Jens Oliver John <jens.o.john@gmail.com>
2011-03-23 19:43:45 +0100

    +Communications with any MPD implemented using coroutines

--- lib/hibiki.lua
+++ lib/hibiki.lua
@@ -1,98 +1,164 @@
 -- hibiki
 -- Music Player Daemon integration for the awesome window manager
--- Version 0.0.0
---
--- Copyright (c) 2011 by Jens Oliver John (2ion) <jens.o.john@gmail.com>
--- Licensed under the GNU General Public License v3.
--- Distributed through http://github.com/2ion/hibiki
+-- Version 0.1.0
 
-local awful = require("awful")
-local naughty = require("naughty")
+-- Copyright (c) 2011 by Jens Oliver John <jens.o.john@gmail.com>
+-- Licensed under the GNU General Public License v3.
+-- Obtainable through https://github.com/2ion/hibiki
+
+-- {{{ separate environment
+local coroutine = coroutine
 local io = io
 local string = string
+local table = table
+local io = io
+local type = type
+local print = print
+local ipairs = ipairs
+local tonumber = tonumber
+local tostring = tostring
+--local awful = require("awful")
+--local naughty = require("naughty")
 module("hibiki")
+-- }}}
 
-local mpd = {}
+-- {{{ private and public tables
+local DAEMONS = {}
+FLAGS = {
+	ready=false
+}
+-- }}}
 
-function init(atable)
-	mpd["server"] = atable["server"] and atable["server"] or "127.0.0.1"
-	mpd["port"] = atable["port"] and atable["port"] or 6600
-	mpd["curl"] = "curl -fsm1 telnet://" .. mpd["server"] .. ":" .. mpd["port"]
-end
-
-function gplaylist()
-	return mpd["playlist"]
-end
-
-function gcurrentsong()
-	return mpd["playlist"][lcurrentsong()]
-end
-
--- displays a popup menu where the track to play will be selected
-function playlistmenu()
-	lplaylist()
-	menuitems = {}
-	for key,value in mpd["playlist"] do
-		menuitems[key] = { value.pos .. value.title, function () pplayfromlist(value.pos) end }
-	end
-	awful.menu.show({ menuitems }, nil, true)
-end
-
-function pplay()
-end
-
-function ppause()
-end
-
-function pstop()
-end
-
-function pnext()
-end
-
-function pprevious()
-end
-
-function pplayfromlist(index)
-end
-
-local function lplaylist()
-	-- parse playlist
-	local pos = 0
-	local file = io.popen("echo playlistinfo |" .. mpd["curl"])
-	for line in file:lines() do
-		for key,value in string.gmatch(line, "([%w]+):[%s](.*)") do
-			if key=="file" then
-				pos=pos+1
-				mpd["playlist"][pos] = { pos=pos, file=value }
-			elseif key=="Time" then mpd["playlist"][pos]["time"]=tonumber(value)
-			elseif key=="Artist" then mpd["playlist"][pos]["artist"]=value
-			elseif key=="Title" then mpd["playlist"][pos]["title"]=value
-			elseif key=="Album" then mpd["playlist"][pos]["album"]=value
-			elseif key=="Track" then mpd["playlist"][pos]["track"]=tonumber(value)
-			elseif key=="Date" then mpd["playlist"][pos]["date"]=tonumber(value)
-			elseif key=="Composer" then mpd["playlist"][pos]["composer"]=value
-			elseif key=="Id" then mpd["playlist"][pos]["composer"]=tonumber(value)
+function retrieve_playlist_co(daemon_handle)
+	local coroutines = {}
+	for thread_handle=1,table.maxn(daemon_handle) do
+		table.insert(coroutines, coroutine.create(
+			function (daemon)
+				local position = 0
+				local stream = io.popen("echo playlistinfo |" .. daemon.telnet)
+				daemon.playlist = {}
+				for line in stream:lines() do
+					for key,value in string.gmatch(line, "([%w]+):[%s](.*)") do
+						if key=="file" then
+							position = position + 1
+							daemon.playlist[position] = { pos=position, file=value }
+						elseif key=="Time" then daemon.playlist[position][key] = value
+						elseif key=="Artist" then daemon.playlist[position][key] = value
+						elseif key=="Title" then daemon.playlist[position][key] = value
+						elseif key=="Album" then daemon.playlist[position][key] = value
+						elseif key=="Track" then daemon.playlist[position][key] = value
+						elseif key=="Date" then daemon.playlist[position][key] = value
+						elseif key=="Composer" then daemon.playlist[position][key] = value
+						elseif key=="Id" then daemon.playlist[position][key] = value
+						end
+					end
+				end
+				stream:close()
+				daemon.playlist.last = position
 			end
-		end
+		))
 	end
-	file:close()
-	mpd["playlist"]["last"]=pos
+
+	for key,routine in ipairs(coroutines) do
+		print(coroutine.resume(routine, DAEMONS[table.remove(daemon_handle)]))
+	end
 end
 
-local function lcurrentsong()
-	local position = nil
-	local file = io.popen("echo currentsong |" .. mpd["curl"])
-	for line in file:lines() do
-		for key,value in string.gmatch(line, "([%w]+):[%s](.*)") do
-			if key=="Pos" then
-				position=tonumber(value)
-				break
-			end
-		end
-	end
-	file:close()
-	return position
+function retrieve_playlist(daemon_handle)
+    local queue = {}
+    if type(daemon_handle)=="number" then
+        if not DAEMONS[daemon_handle] then
+			print("here")
+            return nil
+        else
+            table.insert(queue, DAEMONS[daemon_handle])
+        end
+    elseif type(daemon_handle)=="table" then
+        for key,value in ipairs(daemon_handle) do
+            table.insert(queue, DAEMONS[tonumber(value)])
+        end
+    else return nil
+    end    
+    for key,daemon in ipairs(queue) do
+        local position = 0
+        local stream = io.popen("echo playlistinfo |" .. daemon.telnet)
+        for line in stream:lines() do
+            for key,value in string.gmatch(line, "([%w]+):[%s](.*)") do
+                if key=="file" then
+                    daemon.playlist = {}
+                    position = position + 1
+                    daemon.playlist[position] = { pos=position, file=value }
+                elseif key=="Time" then daemon.playlist[position][key] = tonumber(value)
+                elseif key=="Artist" then daemon.playlist[position][key] = value
+                elseif key=="Title" then daemon.playlist[position][key] = value
+                elseif key=="Album" then daemon.playlist[position][key] = value
+                elseif key=="Track" then daemon.playlist[position][key] = value
+                elseif key=="Date" then daemon.playlist[position][key] = value
+                elseif key=="Composer" then daemon.playlist[position][key] = value
+                elseif key=="Id" then daemon.playlist[position][key] = value
+                end
+            end
+        end
+        stream:close()
+        daemon.playlist.last = position
+    end
 end
 
+function retrieve_position(daemon_handle)
+    if not DAEMONS[daemon_handle] then return nil end
+    local position = nil
+    local stream = io.popen("echo currentsong |" .. DAEMONS[daemon_handle].telnet)
+    for line in stream:lines() do
+        for key,value in string.gmatch(line, "([%w]+):[%s](.*)") do
+            if key=="Pos" then
+                position=tonumber(value)
+                break
+            end
+        end
+    end
+    stream:close()
+    return position
+end
+
+function control_playback(daemon_handle, cmd, positional_argument)
+	local cry
+	if positional_argument then
+		cry = cmd .. " " .. tostring(positional_argument)
+	else
+		cry = cmd
+	end
+
+	if tonumber(daemon_handle)==0 then
+		for key,daemon in ipairs(DAEMONS) do
+			io.popen("echo " .. cry .. " |"	.. daemon.telnet)
+		end
+	elseif DAEMONS[daemon_handle] then
+			io.popen("echo " .. cry .. " |" .. DAEMONS[daemon_handle].telnet)
+	end
+	return cmd
+end
+
+function retrieve_daemon_status(daemon_handle)
+	--TODO:Use all the nice coroutines!!!
+end
+
+function init(servers)
+    for key, server in ipairs(servers) do
+	print("init(): server { host=" .. server.host .. ", port=" .. server.port .. " } to register.")
+        table.insert(DAEMONS,
+            {
+                host=server.host and server.host or "127.0.0.1",
+                port=server.port and server.port or "6600",
+                password=server.password and server.password or nil
+            })   
+    end
+    for key, daemon in ipairs(DAEMONS) do
+        if daemon.password then
+            daemon.telnet="curl -fsm1 telnet://" .. daemon.password .. "@" .. daemon.host .. ":" .. daemon.port
+        else
+            daemon.telnet="curl -fsm1 telnet://" .. daemon.host .. ":" .. daemon.port
+        end
+    end
+	FLAGS.ready=true
+end
 
